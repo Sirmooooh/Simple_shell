7fdd9b707a4205611236c0d8a73e664860be2ab8
Ngugiiii <certifiedngugi@gmail.com>
2023-05-19 04:23:34 -0700

    simpleshell

--- alias.c
+++ alias.c
@@ -13,44 +13,43 @@ void print_alias(alias_t *alias);
  * Return: If an error occurs - -1.
  *         Otherwise - 0.
  */
-
 int shell_alias(char **args, char __attribute__((__unused__)) **speed)
 {
-        alias_t *tmp = aliases;
-        int i, mooh = 0;
-        char *val;
+	alias_t *tmp = aliases;
+	int i, mooh = 0;
+	char *val;
 
-        if (args[0] == NULL)
-        {
-                while (tmp)
-                {
-                        print_alias(tmp);
-                        tmp = tmp->next;
-                }
-                return (mooh);
-        }
-        for (i = 0; args[i]; i++)
-        {
-                tmp = aliases;
-                val = _strchr(args[i], '=');
-                if (val == NULL)
-                {
-                        while (tmp)
-                        {
-                                if (_strcmp(args[i], tmp->name) == 0)
-                                {
-                                        print_alias(tmp);
-                                        break;
-                                }
-                                tmp = tmp->next;
-                        }
-                        if (tmp == NULL)
-                                mooh = create_error(args + i, 1);
-                }
-                else
-                        set_alias(args[i], val);
-        }
-        return (mooh);
+	if (args[0] == NULL)
+	{
+		while (tmp)
+		{
+			print_alias(tmp);
+			tmp = tmp->next;
+		}
+		return (mooh);
+	}
+	for (i = 0; args[i]; i++)
+	{
+		tmp = aliases;
+		val = _strchr(args[i], '=');
+		if (val == NULL)
+		{
+			while (tmp)
+			{
+				if (_strcmp(args[i], tmp->name) == 0)
+				{
+					print_alias(tmp);
+					break;
+				}
+				tmp = tmp->next;
+			}
+			if (tmp == NULL)
+				mooh = create_error(args + i, 1);
+		}
+		else
+			set_alias(args[i], val);
+	}
+	return (mooh);
 }
 
 /**
@@ -59,60 +58,57 @@ int shell_alias(char **args, char __attribute__((__unused__)) **speed)
  * @var_name: Name of the alias.
  * @val: Value of the alias.
  */
-
 void set_alias(char *var_name, char *val)
 {
-        alias_t *tmp = aliases;
-        int len, j, k;
-        char *new_val;
+	alias_t *tmp = aliases;
+	int len, j, k;
+	char *new_val;
 
-        *val = '\0';
-        val++;
-        len = _strlen(val) - _strspn(val, "'\"");
-        new_val = malloc(sizeof(char) * (len + 1));
-        if (new_val == NULL)
-                return;
-        for (j = 0, k = 0; val[j]; j++)
-        {
-                if (val[j] != '\'' && val[j] != '"')
-                        new_val[k++] = val[j];
-        }
-        new_val[k] = '\0';
-        while (tmp)
-        {
-                if (_strcmp(var_name, tmp->name) == 0)
-                {
-                        free(tmp->val);
-                        tmp->val = new_val;
-                        break;
-                }
-                tmp = tmp->next;
-        }
-        if (tmp == NULL)
-                add_alias_end(&aliases, var_name, new_val);
+	*val = '\0';
+	val++;
+	len = _strlen(val) - _strspn(val, "'\"");
+	new_val = malloc(sizeof(char) * (len + 1));
+	if (new_val == NULL)
+		return;
+	for (j = 0, k = 0; val[j]; j++)
+	{
+		if (val[j] != '\'' && val[j] != '"')
+			new_val[k++] = val[j];
+	}
+	new_val[k] = '\0';
+	while (tmp)
+	{
+		if (_strcmp(var_name, tmp->name) == 0)
+		{
+			free(tmp->val);
+			tmp->val = new_val;
+			break;
+		}
+		tmp = tmp->next;
+	}
+	if (tmp == NULL)
+		add_alias_end(&aliases, var_name, new_val);
 }
-
 
 /**
  * print_alias - Prints the alias in the format name='val'.
  * @alias: Pointer to an alias.
  */
-
 void print_alias(alias_t *alias)
 {
-        char *alias_str;
-        int len = _strlen(alias->name) + _strlen(alias->val) + 4;
+	char *alias_str;
+	int len = _strlen(alias->name) + _strlen(alias->val) + 4;
 
-        alias_str = malloc(sizeof(char) * (len + 1));
-        if (alias_str == NULL)
-                return;
-        _strcpy(alias_str, alias->name);
-        _strcat(alias_str, "='");
-        _strcat(alias_str, alias->val);
-        _strcat(alias_str, "'\n");
+	alias_str = malloc(sizeof(char) * (len + 1));
+	if (alias_str == NULL)
+		return;
+	_strcpy(alias_str, alias->name);
+	_strcat(alias_str, "='");
+	_strcat(alias_str, alias->val);
+	_strcat(alias_str, "'\n");
 
-        write(STDOUT_FILENO, alias_str, len);
-        free(alias_str);
+	write(STDOUT_FILENO, alias_str, len);
+	free(alias_str);
 }
 
 /**
@@ -122,38 +118,37 @@ void print_alias(alias_t *alias)
  *
  * Return: 2D pointer to the arguments.
  */
-
 char **replace_aliases(char **args)
 {
-        alias_t *tmp;
-        int j;
-        char *new_val;
+	alias_t *tmp;
+	int j;
+	char *new_val;
 
-        if (_strcmp(args[0], "alias") == 0)
-                return (args);
-        for (j = 0; args[j]; j++)
-        {
-                tmp = aliases;
-                while (tmp)
-                {
-                        if (_strcmp(args[j], tmp->name) == 0)
-                        {
-                                new_val = malloc(sizeof(char) * (_strlen(tmp->val) + 1));
-                                if (new_val == NULL)
-                                {
-                                        free_args(args, args);
-                                        return (NULL);
-                                }
-                                _strcpy(new_val, tmp->val);
-                                free(args[j]);
-                                args[j] = new_val;
-                                j--;
-                                break;
-                        }
-                        tmp = tmp->next;
-                }
-        }
+	if (_strcmp(args[0], "alias") == 0)
+		return (args);
+	for (j = 0; args[j]; j++)
+	{
+		tmp = aliases;
+		while (tmp)
+		{
+			if (_strcmp(args[j], tmp->name) == 0)
+			{
+				new_val = malloc(sizeof(char) * (_strlen(tmp->val) + 1));
+				if (new_val == NULL)
+				{
+					free_args(args, args);
+					return (NULL);
+				}
+				_strcpy(new_val, tmp->val);
+				free(args[j]);
+				args[j] = new_val;
+				j--;
+				break;
+			}
+			tmp = tmp->next;
+		}
+	}
 
-        return (args);
+	return (args);
 }
 
